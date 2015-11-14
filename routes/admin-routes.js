573e74009f19a63182c29ef1cb68535137ab04ad
egendreau <egendreau254@gmail.com>
2015-11-14 15:50:52 -0500

    created strucutre for admin and user routes

---
+++ routes/admin-routes.js
@@ -0,0 +1,7 @@
+var express = require('express');
+
+// This gives us access to the user "model".
+var model = require('../lib/user');
+
+// creates an express "router"
+var router = express.Router();
